826ea8cad7c7c338ffd72ae734d0f2d2045585ae
anamol04 <sh.anmol23@ncmt.edu.np>
2025-01-29 20:39:17 +0545

    FrontEnding

---
+++ script.js
@@ -0,0 +1,6 @@
+let body= document.querySelector("body");
+body.style.backgroundColor="rgb(100, 190, 168)";
+let box= document.querySelectorAll(".box");
+box.forEach(box=>{
+    box.style.backgroundColor="rgba(110, 110, 100, 2)";
+});
