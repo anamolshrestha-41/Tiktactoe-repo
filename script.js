041b00ba55f856ff26f3c84a8855cb26483ff93b
anamol04 <sh.anmol23@ncmt.edu.np>
2025-03-03 20:58:55 +0545

    SOUND ADDED

--- script.js
+++ script.js
@@ -1,55 +1,78 @@
-//Accessing part
-let box= document.querySelectorAll(".box");
-let resetButton = document.querySelector("#game-reset");
-let turn = true;
+let boxes= document.querySelectorAll(".box");
+let reset= document.querySelector("#game-reset");
+let turn=true;
 
-//Winners need to Have These Patterns
+//Winning Patterns in TikTacToe
 const winStrategy=[
     [0, 1, 2],[0,3, 6],[0, 4, 8],[1, 4, 7],
     [2, 5, 8],[3, 4, 5],[6, 7, 8],[2, 4, 6]
 ];
 
-//Adding Eventlistener for each buttons
-box.forEach((boxes)=>{
-    boxes.addEventListener("click",()=>{
+//sounds accessing
+const click = new Audio("/sound/click.mp3");
+const winSound = new Audio("/sound/Win.mp3");
+const bgSound = new Audio("/sound/backGround.mp3");
+
+bgSound.loop=true;
+bgSound.volume=0.01;
+
+window.addEventListener("load", ()=>{
+    bgSound.play();
+} );
+
+//Adding Event Listeners for each buttons to access and sound.
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        click.currentTime = 0;
+            click.play();
         if(turn){
-            boxes.innerText="O";
+            box.innerText="O";
             turn=false;
         }
         else{
-            boxes.innerText="X";
+            box.innerText="X";
             turn= true;
         }
-        // boxes.disabled=true;
-        boxes.style.pointerEvents = "none"; // Disable clicks
+        box.disabled=true;
         checkWinner();
     });
 });
 
-//Function for checking winner
-function checkWinner(){
-    for(let strategy of winStrategy){
-        let pos1 = box[strategy[0]].innerText;
-        let pos2 = box[strategy[1]].innerText;
-        let pos3 = box[strategy[2]].innerText;
-        if(pos1==pos2 && pos2==pos3 && pos1!=""){
-            setTimeout(()=>{
-                alert(`Player ${pos1} wins!!`);
-                resetGame();
+//Function To Check Winner
+ function checkWinner(){
+    for(let Strategy of winStrategy){
+        let pos1= boxes[Strategy[0]].innerText;
+        let pos2= boxes[Strategy[1]].innerText;
+        let pos3= boxes[Strategy[2]].innerText;
+        if (pos1 === pos2 && pos2 === pos3 && pos1 !== "") {
+            setTimeout(() => { //setTimeout is a built-in JavaScript function that
+            //  delays the execution of a function for a specified time (in milliseconds).
+            winSound.play(); 
+            alert(`Player ${pos1} wins!`);
+                resetGame(); // Auto-restart after showing alert
             });
             return;
         }
+
+
+        //Practice Code!!....
+        // let [a, b, c]= winStrategy[i];
+        // if(boxes[a].innerText === boxes[b].innerText && boxes[b].innerText === boxes[c]){
+        //     alert(`Player ${boxes[a].innerText} wins!`);
+        //     resetGame();
+        // }
     }
-};
+ };
 
-//function for reset game
-function resetGame(){
-    box.forEach((boxes)=>{
-        boxes.innerText="";
-        boxes.style.pointerEvents = "auto";
+ //Function To Reset The Game
+ function resetGame() {
+    setTimeout(() => {
+        boxes.forEach((box) => {
+            box.innerText = "";
+            box.disabled = false;
         });
-        turn=true;
-    };
-
-//Event Listener for Game Changer... hahahaha
-resetButton.addEventListener("click", resetGame);
+        turn = true;
+ }); 
+}
+//Adding eventListner to the reset button
+ reset.addEventListener("click", resetGame);
